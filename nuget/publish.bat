c94251450fd81971a4a1bdafe0a8f1cfa80d61ce
Ralf Westphal <info@ralfw.de>
2014-03-06 23:53:51 +0100

    Moved .nuspec to proj folder

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,12 +1,12 @@
 cd %srcFolder%\nuget
 
-mkdir lib
-copy ..\lib\mongodb\*.* lib\*.*
-copy ..\lib\sqlite\netFx45Win32\*.* lib\*.*
+rem mkdir lib
+rem copy ..\lib\mongodb\*.* lib\*.*
+rem copy ..\lib\sqlite\netFx45Win32\*.* lib\*.*
 
 nuget pack ..\source\NBlackBox\nblackbox\nblackbox.csproj
 nuget setApiKey %nugetApiKey%
 nuget push nblackbox.%projectVersion%.0.nupkg
 
-del lib\*.* /Q
-rmdir lib
+rem del lib\*.* /Q
+rem rmdir lib
