9b40f38c8242aec241bfa2ea040c22a1eceab537
Ralf Westphal <info@ralfw.de>
2014-03-07 00:27:01 +0100

    Moved supporting assemblies to net45 folder for nuget

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,12 +1,5 @@
 cd %srcFolder%\nuget
 
-rem mkdir lib
-rem copy ..\lib\mongodb\*.* lib\*.*
-rem copy ..\lib\sqlite\netFx45Win32\*.* lib\*.*
-
 nuget pack ..\source\NBlackBox\nblackbox\nblackbox.csproj
 nuget setApiKey %nugetApiKey%
 nuget push nblackbox.%projectVersion%.0.nupkg
-
-rem del lib\*.* /Q
-rem rmdir lib
